4ad43c6d09b25fcfadec2a68af821b684e438ef4
Madhava <madhavamadhava77@gmail.com>
2025-09-17 16:51:26 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "your_password"; 
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
